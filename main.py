c331824bb83e5cb6e9c98d112edac0afcc957722
zakery292 <112213249+zakery292@users.noreply.github.com>
2024-01-05 22:27:48 +0000

    .

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import subprocess
-
+import os
 # Run both scripts simultaneously
 print('Starting the Scripts running simultaneously')
 subprocess.Popen(["python", "soc_collections.py"])
 subprocess.Popen(["python", "predict_soc.py"])
 
+
+# Print all environment variables
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
 # Keep the main script running
 while True:
     pass
