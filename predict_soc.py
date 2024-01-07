6447f866fa8e3b85bcc4db227107155fdde5aa7f
zakery292 <112213249+zakery292@users.noreply.github.com>
2024-01-07 18:48:19 +0000

    .

--- predict_soc.py
+++ predict_soc.py
@@ -88,7 +88,7 @@ def train_model(df):
     X = df[features]
     y = df["soc"]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=60)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.6, random_state=120)
     model = HistGradientBoostingRegressor(random_state=42)
     model.fit(X_train, y_train)
 
